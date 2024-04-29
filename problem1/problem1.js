d18d7266e0fa14bea0673f1f8d4a9535d0dd875c
vynguyen20121997 <133662722+vynguyen20121997@users.noreply.github.com>
2024-04-29 19:44:44 +0700

    problem1 updating

--- problem1/problem1.js
+++ problem1/problem1.js
@@ -7,17 +7,14 @@ var sum_to_n_a = function (n) {
 };
 
 var sum_to_n_b = function (n) {
-  let result = 0;
-
-  return -result;
+  const result = new Array(n + 1)
+    .fill(0)
+    .map((e, index) => index)
+    .reduce((a, b) => a + b);
+  return result;
 };
 
 var sum_to_n_c = function (n) {
   let k = (n - 0) / 1 + 1;
   return ((0 + n) * k) / 2;
 };
-
-let sum1 = sum_to_n_a(4);
-let sum2 = sum_to_n_b(4);
-let sum3 = sum_to_n_c(4);
-console.log(sum1, sum2, sum3);
